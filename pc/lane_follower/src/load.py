90b51f6f871b469b8f318f2f630d4d057afcb074
guru <59327681+guru-narayana@users.noreply.github.com>
2020-10-07 15:14:22 +0530

    Update load.py

--- pc/lane_follower/src/load.py
+++ pc/lane_follower/src/load.py
@@ -8,15 +8,11 @@ lr = 1e-3
 model = alexnet2(WIDTH,HEIGHT,lr,output=4)
 model.load('final.model')
 def predict_image(image):
+    out = [0,0]
     im = image.reshape(150,100,1)
     prediction = np.array(model.predict([im]))
-    prediction = (prediction > 0.4)*1
+    print(prediction)
+    out[0] = prediction[0][0]*2
+    out[1] = prediction[0][1]*2+prediction[0][2]*(-2)
     print(prediction[0])
-    if (prediction[0] == [1,0,0,0]).all():
-        return([2,0])
-    elif(prediction[0] == [0,0,1,0]).all():
-        return([0,-2])
-    elif (prediction[0] == [0,1,0,0]).all():
-        return([0,2])
-    else:
-        return([0,0])
+    return(out)
